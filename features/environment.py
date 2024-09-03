36341d2758eec12bb09746e579bc4b2db6da5442
Sonya Potha <sonyapotha98@gmail.com>
2024-09-03 13:12:44 -0700

    BrowserStack Implementation

--- features/environment.py
+++ features/environment.py
@@ -7,8 +7,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 
 
-
-def browser_init(context):
+def browser_init(context,scenario_name):
     """
     :param context: Behave context
     """
@@ -17,10 +16,12 @@ def browser_init(context):
     # service = Service(driver_path)
     # context.driver = webdriver.Chrome(service=service)
 
-    ### FIREFOX ####
-    driver_path = GeckoDriverManager().install()
-    service = Service(driver_path)
-    context.driver = webdriver.Firefox(service=service)
+
+    # ### FIREFOX ####
+    # driver_path = GeckoDriverManager().install()
+    # service = Service(driver_path)
+    # context.driver = webdriver.Firefox(service=service)
+
 
     ### HEADLESS MODE ####
     # options = webdriver.ChromeOptions()
@@ -31,6 +32,25 @@ def browser_init(context):
     #     service=service
     # )
 
+
+    ### BROWSERSTACK ###
+    bs_user = 'sonyapotha_IDjqPV'
+    bs_key = '8WixNvQ43Xx7y3oyJYp3'
+    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+
+    options = Options()
+    bstack_options = {
+        "os": "Windows",
+        "osVersion": "11",
+        'browserName': 'firefox',
+        'sessionName': scenario_name
+    }
+
+    options.set_capability('bstack:options', bstack_options)
+    context.driver = webdriver.Remote(command_executor=url, options=options)
+
+    # context.driver.set_window_size(1920, 1080)
+
     context.driver.maximize_window()
     context.driver.implicitly_wait(4)
     context.driver.wait = WebDriverWait(context.driver, 10)
@@ -39,7 +59,7 @@ def browser_init(context):
 
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
-    browser_init(context)
+    browser_init(context, scenario.name)
 
 
 def before_step(context, step):
